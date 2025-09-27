483431bce5f215bc2d8e429a9bdec9f6553a03ad
KahoanDev <kahoan9@gmail.com>
2025-09-26 23:55:54 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/KahoanDev/poc_crud_api/PocCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.KahoanDev.poc_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocCrudApiApplication.class, args);
+	}
+
+}
